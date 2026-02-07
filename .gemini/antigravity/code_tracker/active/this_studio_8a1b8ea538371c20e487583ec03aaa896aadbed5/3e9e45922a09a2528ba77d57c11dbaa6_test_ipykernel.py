bcd3074d3ff04a6440b2b9b4dcadf9bb83165e1f
Soyoung Yun <yunsoyoung1107@gmail.com>
2026-02-08 00:33:40 +0900

    260207 - Exclude large data and model files

--- .gemini/antigravity/code_tracker/active/this_studio_8a1b8ea538371c20e487583ec03aaa896aadbed5/3e9e45922a09a2528ba77d57c11dbaa6_test_ipykernel.py
+++
@@ -1,6 +0,0 @@
-{
-import sys
-from ipykernel import kernelapp as app
-print(f"Python version: {sys.version}")
-print("Ipykernel is available")
-{"(8a1b8ea538371c20e487583ec03aaa896aadbed527file:///teamspace/studios/this_studio/test_ipykernel.py:%file:///teamspace/studios/this_studio
